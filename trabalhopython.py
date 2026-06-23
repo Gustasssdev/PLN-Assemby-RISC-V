3f5764bae4dd73cf17c0b5f381d816707a553093
Diogo Rangel <diogo.rangelgoncalves09@gmail.com>
2026-06-22 22:07:14 -0300

    Refactor text processing and repetition detection

--- trabalhopython.py
+++ trabalhopython.py
@@ -1,51 +1,43 @@
 #Trabalho de Arquitetura:
-import re
+texto = "marcos. marcos. julia julia e thiago thiago thiago sao da fisica fisica experimental"
 i = 0
-resultado = None
-pontuacao = 1
-palavras_repetidas = dict()
-texto = "luz luz e diogo,diogo,gustavo GUSTAVO sao da computacao"
-lista_separada = re.split(r'(\W+)', texto)
-pontuacoes = re.findall(r'[^\w\s]', texto)
-msg_tratada = [item.strip() for item in lista_separada if item.strip()]
+palavra_prev = ""
+palavra_atual = ""
+texto_final = ""
+resultado = ""
+palavras_repetidas = list()
 
-def position(text, palavra, segunda_aparicao):
-    inicio = -1
-    for i in range(segunda_aparicao):
-        inicio = text.find(palavra, inicio + 1)
-        if inicio == -1:
-            return 0
-    return inicio
-
-for palavra in msg_tratada:
-     if i == 0:
-            palavra_prev = palavra
-            msg_final = palavra
-     else:
-        if len(palavra) == 1:
-            if palavra in pontuacoes and pontuacao == 1:
-                msg_final += palavra
-                pontuacao = 0
+for letra in texto:
+    if (ord(letra) >= 32 and ord(letra) <= 47) or (ord(letra) >= 58 and ord(letra) <= 64) or (ord(letra) >= 91 and ord(letra) <= 96) or (ord(letra) >= 123 and ord(letra) <=127):
+        if palavra_atual.lower() == palavra_prev.lower():
+            if palavra_atual.capitalize()  not in palavras_repetidas:
+                palavras_repetidas.append(palavra_atual.capitalize())
+            palavra_atual = ""
         else:
-            palavra_atual = palavra
-            if palavra_prev.lower() != palavra_atual.lower():
-                msg_final += f" {palavra}"
-                palavra_prev = palavra
-                pontuacao = 1
-            else:
-                pos = position(texto.lower(), palavra.lower(), 2)
-                palavras_repetidas[palavra.lower()] = pos + 1
-     i += 1
-
-#Percorrer a lista das palavras repetidas:
-i = 0
-for word in palavras_repetidas:
-    if i == 0:
-        resultado = f"{word.capitalize()} - {palavras_repetidas[word]}"
+            texto_final += palavra_atual + letra
+            if i == 0:
+                palavra_prev = palavra_atual
+                palavra_atual = ""
+            i = 1
     else:
-        resultado += f" {word.capitalize()} - {palavras_repetidas[word]}"
-    i += 1
+        palavra_atual += letra
+        i = 0
+
+if palavra_atual.lower() == palavra_prev.lower():
+    if palavra_atual.capitalize()  not in palavras_repetidas:
+            palavras_repetidas.append(palavra_atual.capitalize())
+else:        
+    texto_final += palavra_atual
+
+for word in palavras_repetidas:
+    resultado += f"{word} "
+
+
+print(texto_final)
+print(palavras_repetidas)
+
 
 with open("Resultado.txt", "w") as file:
-    file.write(f"{msg_final}\n")
+    file.write(f"{texto_final}\n")
     file.write(f"{resultado}\n")
+
